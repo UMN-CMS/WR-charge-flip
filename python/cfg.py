1d2acac9c0c2bc1a68f955a6e96c8bb26b1ea4cc
Martin Meier <meier307@umn.edu>
2022-08-04 16:07:46 -0500

    CMSSW charge flip analysis config file added

--- python/cfg.py
+++ python/cfg.py
@@ -1,60 +1,25 @@
 import FWCore.ParameterSet.Config as cms
 import HLTrigger.HLTfilters.hltHighLevel_cfi
 from FWCore.ParameterSet.VarParsing import VarParsing
+from PhysicsTools.PatAlgos.tools.jetTools import *
 import os
 #INPUT PARSING SECTION
 options = VarParsing ('analysis')
 
-options.register( 'isSignal',
-                  False,
-                  VarParsing.multiplicity.singleton,
-                  VarParsing.varType.bool,
-                  "True when running over signal MC samples"
-               )
 
-options.register( 'genTrainData',
-				  False,
-				  VarParsing.multiplicity.singleton,
-				  VarParsing.varType.bool,
-				  "True when generating a training set for neural network"
-				   )	   
-		   
+options.register( 'nEvents',
+                  -1,
+                  VarParsing.multiplicity.singleton,
+                  VarParsing.varType.int,
+                  "Number of events. Default is all events (-1)"
+               )  
+           
 options.register( 'trainFile',
-				  'nn.txt',
-				  VarParsing.multiplicity.singleton,
-				  VarParsing.varType.string,
-				  "Name of file for training set for neural network"
-				   )
-options.register ('gitTag',
-				  'applePie',
-				  VarParsing.multiplicity.singleton,
-				  VarParsing.varType.string,
-				  "Git Tag")
-
-options.register( 'isMC',
-				  True,
-				  VarParsing.multiplicity.singleton,
-				  VarParsing.varType.bool,
-				  "True if is MC dataset")
-
-options.register( 'ISmcatnlo',
-                  False,
-                  VarParsing.multiplicity.singleton,
-                  VarParsing.varType.bool,
-                  "True if is MC @ NLO dataset")
-
-options.register( 'doFast',
-                  False,
-                  VarParsing.multiplicity.singleton,
-                  VarParsing.varType.bool,
-                  "True to run a condensed analysis for HiggsCombine"
-               )
-
-options.register( 'reco',
-                  '80X',
+                  'nn.csv',
                   VarParsing.multiplicity.singleton,
                   VarParsing.varType.string,
-                  "MC or data CMSSW reco")
+                  "Name of csv file for saving training data for neural network"
+                   )
 
 options.register( 'era',
                   '2016',
@@ -63,11 +28,11 @@ options.register( 'era',
                   "Year of Run II")
 
 options.register( 'checkZ',
-		  True,
-		  VarParsing.multiplicity.singleton,
-		  VarParsing.varType.bool,
-		  "True when running over Drell-Yan MC samples"
-	       )
+          True,
+          VarParsing.multiplicity.singleton,
+          VarParsing.varType.bool,
+          "True when running over Drell-Yan MC samples"
+           )
 options.parseArguments()
 
 #LOCAL VARIABLE DEFINITIONS
@@ -88,15 +53,28 @@ process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condD
 from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
 from Configuration.AlCa.autoCond import autoCond
 
+#get Electron MVA value map
+process.load("RecoEgamma.ElectronIdentification.ElectronMVAValueMapProducer_cfi")
+
 process.GlobalTag = GlobalTag(process.GlobalTag, '94X_mcRun2_asymptotic_v3') #
 if not options.isMC: process.GlobalTag = GlobalTag(process.GlobalTag, '94X_dataRun2_v10')
 
 
-
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(options.nEvents) )
 process.source = cms.Source ("PoolSource",
-	  fileNames = cms.untracked.vstring (options.inputFiles),
+      fileNames = cms.untracked.vstring ( #additional input filepaths located at data/ttbar_file_list.txt
+      'root://cmsxrootd.fnal.gov//store/mc/RunIIAutumn18MiniAOD/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/MINIAODSIM/102X_upgrade2018_realistic_v15-v1/110000/079C3FC4-8835-394B-8E54-1C67DFAE7E8D.root',
+      'root://cmsxrootd.fnal.gov//store/mc/RunIIAutumn18MiniAOD/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/MINIAODSIM/102X_upgrade2018_realistic_v15-v1/110000/0B4D0775-CC78-904D-A4B0-6B755608ABB5.root',
+      'root://cmsxrootd.fnal.gov//store/mc/RunIIAutumn18MiniAOD/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/MINIAODSIM/102X_upgrade2018_realistic_v15-v1/110000/0D86B53B-2397-EE40-9A96-8115D6A754C2.root',
+      'root://cmsxrootd.fnal.gov//store/mc/RunIIAutumn18MiniAOD/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/MINIAODSIM/102X_upgrade2018_realistic_v15-v1/110000/0F1A6FA9-1D64-3E4D-B6CE-69EC5C1462C9.root',
+      'root://cmsxrootd.fnal.gov//store/mc/RunIIAutumn18MiniAOD/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/MINIAODSIM/102X_upgrade2018_realistic_v15-v1/110000/1C576984-A8C6-B348-97FB-EEDC216ABDBD.root',
+      'root://cmsxrootd.fnal.gov//store/mc/RunIIAutumn18MiniAOD/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/MINIAODSIM/102X_upgrade2018_realistic_v15-v1/110000/2498CC8E-233D-EE4B-91E1-467862BB453A.root',
+      'root://cmsxrootd.fnal.gov//store/mc/RunIIAutumn18MiniAOD/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/MINIAODSIM/102X_upgrade2018_realistic_v15-v1/110000/29C48882-452F-C14F-AFC9-C107D0623F83.root',
+      'root://cmsxrootd.fnal.gov//store/mc/RunIIAutumn18MiniAOD/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/MINIAODSIM/102X_upgrade2018_realistic_v15-v1/110000/3AA76E9B-B61B-0647-957D-B698FA7C972A.root',
+      'root://cmsxrootd.fnal.gov//store/mc/RunIIAutumn18MiniAOD/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/MINIAODSIM/102X_upgrade2018_realistic_v15-v1/110000/52713434-77E4-EA4F-822F-3B3AE54C4E03.root',
+      'root://cmsxrootd.fnal.gov//store/mc/RunIIAutumn18MiniAOD/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/MINIAODSIM/102X_upgrade2018_realistic_v15-v1/110000/80E49E44-DFA3-C945-A509-521815C9808B.root',
+      )  
 )
-
 
 
 process.options = cms.untracked.PSet(
@@ -127,8 +105,8 @@ process.removeBadAndCloneGlobalMuons = cms.EDProducer("MuonRefPruner",
 )
 
 process.tunePMuons = cms.EDProducer("TunePMuonProducer",
-		src = cms.InputTag("removeBadAndCloneGlobalMuons")
-		#src = cms.InputTag("slimmedMuons")
+        src = cms.InputTag("removeBadAndCloneGlobalMuons")
+        #src = cms.InputTag("slimmedMuons")
 )
 
 ### muon ID and isolation
@@ -137,6 +115,8 @@ process.tuneIDMuons = cms.EDFilter("PATMuonSelector",
                                src = cms.InputTag("tunePMuons"),
                                cut = cms.string(muonID),
 )
+
+
 #HERE WE RUN A MODULE FROM SAM HARPER WHICH INSERTS HEEP CUT INFO INTO THE PAT ELECTRON USER DATA
 #we setup the HEEP ID V7.0 and enable VID via the following function
 #and then add it to a new collection of pat::Electrons
@@ -160,56 +140,95 @@ process.printTree = cms.EDAnalyzer("ParticleTreeDrawer",
                                    printPtEtaPhi = cms.untracked.bool(False),
                                    printVertex = cms.untracked.bool(False),
                                    printStatus = cms.untracked.bool(False),
-                                   printIndex = cms.untracked.bool(True),
-#                                   status = cms.untracked.vint32( 3 )
+                                   printIndex = cms.untracked.bool(False),
+                                   #status = cms.untracked.vint32( 3 )
                                    )
 
 #process.p = cms.Path(
 #    process.heepSequence*
 #    process.heepIdExample) #our analysing example module, replace with your module
-#
 
 process.muonSelectionSeq = cms.Sequence(cms.ignore(process.badGlobalMuonTagger) * cms.ignore(process.cloneGlobalMuonTagger) * process.removeBadAndCloneGlobalMuons * process.tunePMuons * process.tuneIDMuons)
 
 muonPaths = cms.vstring("HLT_Mu50_v", "HLT_TkMu50_v")
 electronPaths = cms.vstring("HLT_Ele27_WPTight_Gsf_v", "HLT_Photon175_v")
 if options.era == '2016':
-	muonPaths = cms.vstring("HLT_Mu50_v", "HLT_TkMu50_v")
-	electronPaths = cms.vstring("HLT_Ele27_WPTight_Gsf_v", "HLT_Ele115_CaloIdVT_GsfTrkIdT_v", "HLT_Photon175_v")
+    muonPaths = cms.vstring("HLT_Mu50_v", "HLT_TkMu50_v")
+    electronPaths = cms.vstring("HLT_Ele27_WPTight_Gsf_v", "HLT_Ele115_CaloIdVT_GsfTrkIdT_v", "HLT_Photon175_v")
 elif options.era == '2017':
-	muonPaths = cms.vstring("HLT_Mu50_v", "HLT_OldMu100", "HLT_TkMu100")
-	electronPaths = cms.vstring("HLT_Ele35_WPTight_Gsf_v","HLT_Photon200_v","HLT_Ele115_CaloIdVT_GsfTrkIdT_v")
+    muonPaths = cms.vstring("HLT_Mu50_v", "HLT_OldMu100", "HLT_TkMu100")
+    electronPaths = cms.vstring("HLT_Ele35_WPTight_Gsf_v","HLT_Photon200_v","HLT_Ele115_CaloIdVT_GsfTrkIdT_v")
 elif options.era == '2018':
-	muonPaths = cms.vstring("HLT_Mu50", "HLT_OldMu100", "HLT_TkMu100")
-	electronPaths = cms.vstring("HLT_Ele32WPTight_Gsf_v","HLT_Photon200_v","HLT_Ele115_CaloIdVT_GsfTrkIdT_v")
+    muonPaths = cms.vstring("HLT_Mu50", "HLT_OldMu100", "HLT_TkMu100")
+    electronPaths = cms.vstring("HLT_Ele32WPTight_Gsf_v","HLT_Photon200_v","HLT_Ele115_CaloIdVT_GsfTrkIdT_v")
 
-process.analysis = cms.EDAnalyzer('WR_MASS_PLOT',
-						tracks = cms.untracked.InputTag('ctfWithMaterialTracks'),
-						genParticles = cms.InputTag("prunedGenParticles"),
-						AK4recoCHSJets = cms.InputTag("slimmedJets"),
-						highMuons = cms.InputTag("tuneIDMuons"),
-						highElectrons = cms.InputTag("heepElectrons"),
-						genInfo = cms.InputTag("generator"),
-						vertices = cms.InputTag("offlineSlimmedPrimaryVertices"),
-						trainFile = cms.untracked.string(options.trainFile),
-						isSignal = cms.untracked.bool(options.isSignal),
-						genTrainData = cms.untracked.bool(options.genTrainData)
+# process.muonpPtFilter = cms.EDFilter("MCSingleParticleFilter",
+#     MaxEta = cms.untracked.vdouble(2.4, 2.4),
+#     Status = cms.untracked.vint32(1,  1),
+#     MinEta = cms.untracked.vdouble(-2.4, -2.4),
+#     MinPt = cms.untracked.vdouble(200, 200),
+#     ParticleID = cms.untracked.vint32(13, -13)
+# )
+
+process.muonpPtFilter = cms.EDFilter("muonFilter",
+      ptMin = cms.double(400),
+      genParticles = cms.InputTag("prunedGenParticles"),
 )
 
+process.analysis = cms.EDAnalyzer('NNstudies',
+                        tracks = cms.untracked.InputTag('ctfWithMaterialTracks'),
+                        genParticles = cms.InputTag("prunedGenParticles"),
+                        AK4genCHSJets = cms.InputTag("slimmedGenJets"),
+                        AK4CHSJets = cms.InputTag("slimmedJets"),
+                        # highMuons = cms.InputTag("tuneIDMuons"),
+                        highMuons = cms.InputTag("slimmedMuons"),
+                        # highElectrons = cms.InputTag("heepElectrons"),
+                        highElectrons = cms.InputTag("slimmedElectrons"),
+                        trigResults = cms.InputTag("TriggerResults","","HLT"),
+                        genInfo = cms.InputTag("generator"),
+                        vertices = cms.InputTag("offlineSlimmedPrimaryVertices"),
+                        trainFile = cms.untracked.string(options.trainFile),
+                        genTrainData = cms.untracked.bool(options.genTrainData),
+                        electronPathsToPass = electronPaths,
+                        recoMET = cms.InputTag("slimmedMETs"),
+                        packedGenParticles = cms.InputTag("packedGenParticles"),
+                        packedPFCandidates = cms.InputTag("packedPFCandidates"),
+                        mvaValuesMap = cms.InputTag("electronMVAValueMapProducer:ElectronMVAEstimatorRun2Fall17IsoV2RawValues"),
+                        rho = cms.InputTag("fixedGridRhoFastjetAll")
+
+)
 
 
 process.load('FWCore.Modules.printContent_cfi')
 
 from PhysicsTools.SelectorUtils.tools.vid_id_tools import *
-	
+
+# mva stuff
+process.load('JetMETCorrections.Configuration.JetCorrectors_cff')
+
+dataFormat = DataFormat.MiniAOD
+switchOnVIDElectronIdProducer(process, dataFormat)
+my_id_modules = ['RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Fall17_iso_V2_cff']
+for idmod in my_id_modules:
+      setupAllVIDIdsInModule(process,idmod,setupVIDElectronSelection)
+
+from RecoJets.JetProducers.fixedGridRhoProducerFastjet_cfi import fixedGridRhoFastjetAll
+
+process.fixedGridRhoFastjetAll = cms.EDProducer("FixedGridRhoProducerFastjet",
+    pfCandidatesTag = cms.InputTag("packedPFCandidates"),
+    maxRapidity = cms.double(5.0),
+    gridSpacing = cms.double(0.55)
+)
+    
 process.selectedElectrons = cms.EDFilter("PATElectronSelector",
     src = cms.InputTag("slimmedElectrons"),
     cut = cms.string("pt>5 && abs(eta)")
 )
-process.egmGsfElectronIDs.physicsObjectSrc = cms.InputTag('selectedElectrons')
-process.heepIDVarValueMaps.elesMiniAOD  = 'selectedElectrons'
-process.electronMVAValueMapProducer.srcMiniAOD = cms.InputTag('selectedElectrons')
-process.heepElectrons.src = cms.InputTag('selectedElectrons')
+
+# process.egmGsfElectronIDs.physicsObjectSrc = cms.InputTag('selectedElectrons')
+# process.heepIDVarValueMaps.elesMiniAOD  = 'selectedElectrons'
+# process.electronMVAValueMapProducer.srcMiniAOD = cms.InputTag('selectedElectrons')
+# process.heepElectrons.src = cms.InputTag('selectedElectrons')
 
 ####EE L1 Prefiring Correction ####
 from PhysicsTools.PatUtils.l1ECALPrefiringWeightProducer_cfi import l1ECALPrefiringWeightProducer
@@ -233,10 +252,5 @@ elif options.era == '2018':
       SkipWarnings = False)
 
 
-
-#process.totalPath = cms.Path(process.selectedElectrons * process.heepSequence
-#                           * process.muonSelectionSeq * process.analysis )#* process.printTree)
-process.totalPath = cms.Path(process.selectedElectrons * process.heepSequence
-                           * process.muonSelectionSeq * process.prefiringweight * process.analysis )#* process.printTree)
-
-
+process.totalPath = cms.Path(process.selectedElectrons  * process.egmGsfElectronIDSequence * process.fixedGridRhoFastjetAll #* process.heepSequence 
+                           * process.muonSelectionSeq * process.analysis )# * process.printTree)
